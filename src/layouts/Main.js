af5149382049a4e391d9bb5837b7d51bb0e1b8ef
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2021-06-09 14:34:35 +0900

    layout of main

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { makeStyles } from "@material-ui/styles";
 
 const Main = () => {
-  return <div></div>;
+  const classes = useStyle();
+  return (
+    <div className={classes.mainBox}>
+      <h1 className={classes.h1}>Main</h1>
+    </div>
+  );
 };
+
+const useStyle = makeStyles((theme) => ({
+  mainBox: {
+    backgroundColor: "#F5F5DC",
+    height: "70vh",
+    width: "85vw",
+    float: "right"
+  },
+  h1: {
+    color: "black",
+    paddingTop: "35%",
+    paddingLeft: "45%"
+  }
+}));
 
 export default Main;
