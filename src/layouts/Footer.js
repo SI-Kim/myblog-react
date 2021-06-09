be5a644091665d7877639184bdb57bd07d279c24
SI-Kim <48465526+SI-Kim@users.noreply.github.com>
2021-06-09 14:29:15 +0900

    layout of sidbar and footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { makeStyles } from "@material-ui/styles";
 
 const Footer = () => {
-  return <div></div>;
+  const classes = useStyle();
+  return (
+    <div className={classes.footerBox}>
+      <p className={classes.p}>Footer</p>
+    </div>
+  );
 };
+
+const useStyle = makeStyles((theme) => ({
+  footerBox: {
+    backgroundColor: "#483D8B",
+    position: "absolute",
+    bottom: "0",
+    width: "100%",
+    height: "15%",
+    color: "white"
+  },
+  p: {
+    paddingLeft: "4%",
+    paddingTop: "3%"
+  }
+}));
 
 export default Footer;
--- src/layouts/Sidebar.js
+++ src/layouts/Sidebar.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { makeStyles } from "@material-ui/styles";
 
 const Sidebar = () => {
-  return <div></div>;
+  const classes = useStyle();
+  return (
+    <div className={classes.sidebarBox}>
+      <p className={classes.p}>Sidebar</p>
+    </div>
+  );
 };
+
+const useStyle = makeStyles((theme) => ({
+  sidebarBox: {
+    backgroundColor: "#3CB371",
+    width: "15%",
+    color: "white",
+    height: "70%",
+    float: "left"
+  },
+  p: {
+    paddingLeft: "20%",
+    paddingTop: "170%"
+  }
+}));
 
 export default Sidebar;
