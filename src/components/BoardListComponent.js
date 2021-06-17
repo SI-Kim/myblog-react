13f889c9ed51006ee29ac3977b9164191d13955e
sung-ilkim <skim88.1942@gmail.com>
2021-06-17 20:25:49 +0900

    data

--- src/components/BoardListComponent.js
+++ src/components/BoardListComponent.js
@@ -10,17 +10,38 @@ import {
 import Posts from "../datas/BoardPosts";
 
 const BoardListComponent = () => {
+  Posts.map((data) => {
+    console.log(data);
+    console.log(data.id.toString());
+    console.log(data.title);
+    console.log(data.date);
+  });
+  console.log(Posts);
   const classes = useStyle();
   return (
     <div className={classes.boardBox}>
       <TableContainer className={classes.table}>
         <TableHead>
           <TableRow>
-           <TableCell></TableCell>
-           <TableCell></TableCell>
-           <TableCell></TableCell>
+            <TableCell align="center" style={{ width: "10%" }}>번호</TableCell>
+            <TableCell align="center" style={{ width: "60%" }}>제목</TableCell>
+            <TableCell align="center" style={{ width: "10%" }}>일시</TableCell>
           </TableRow>
         </TableHead>
+        <TableBody>
+
+          {
+            Posts.map((data) => {
+              return (
+                <TableRow key={data.id}>
+                  <TableCell>{data.id.toString()}</TableCell>
+                  <TableCell align="right" >{data.title}</TableCell>
+                  <TableCell>{data.date}</TableCell>
+                </TableRow>
+              );
+            })
+          }
+        </TableBody>
       </TableContainer>
     </div>
   );
@@ -28,16 +49,15 @@ const BoardListComponent = () => {
 
 const useStyle = makeStyles((theme) => ({
   boardBox: {
-    backgroundColor: "grey",
+    
     width: "100%",
     height: "100%"
   },
   table: {
     marginLeft: "5%",
     marginRight: "5% auto",
-    backgroundColor: "yellow",
     width: "90%",
-    height: "100%"
+    height: "100%",
   }
 }));
 
